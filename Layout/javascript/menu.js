2bc9e60242d0ff5e4166fad71bd72144cd16f45b
Tommye Silva <tommy_vinicius@hotmail.com>
2017-10-25 14:56:31 -0300

    Menu diferenciado
     Responsibilidade

---
+++ Layout/javascript/menu.js
@@ -0,0 +1,154 @@
+(function($) {
+
+    /*
+        Responsive Flat Menu
+        http://cssmenumaker.com/menu/responsive-flat-menu
+    */
+
+    $.fn.menumaker = function(options) {
+
+        var cssmenu = $(this),
+            settings = $.extend({
+                title: "Menu",
+                format: "dropdown",
+                sticky: false
+            }, options);
+
+        return this.each(function() {
+            cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
+            $(this).find("#menu-button").on('click', function() {
+                $(this).toggleClass('menu-opened');
+                var mainmenu = $(this).next('ul');
+                if (mainmenu.hasClass('open')) {
+                    mainmenu.hide().removeClass('open');
+                } else {
+                    mainmenu.show().addClass('open');
+                    if (settings.format === "dropdown") {
+                        mainmenu.find('ul').show();
+                    }
+                }
+            });
+
+            cssmenu.find('li ul').parent().addClass('has-sub');
+
+            multiTg = function() {
+                cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+                cssmenu.find('.submenu-button').on('click', function() {
+                    $(this).toggleClass('submenu-opened');
+                    if ($(this).siblings('ul').hasClass('open')) {
+                        $(this).siblings('ul').removeClass('open').hide();
+                    } else {
+                        $(this).siblings('ul').addClass('open').show();
+                    }
+                });
+            };
+
+            if (settings.format === 'multitoggle') multiTg();
+            else cssmenu.addClass('dropdown');
+
+            if (settings.sticky === true) cssmenu.css('position', 'fixed');
+
+            resizeFix = function() {
+                if ($(window).width() > 768) {
+                    cssmenu.find('ul').show();
+                }
+
+                if ($(window).width() <= 768) {
+                    cssmenu.find('ul').hide().removeClass('open');
+                }
+            };
+            resizeFix();
+            return $(window).on('resize', resizeFix);
+
+        });
+    };
+})(jQuery);
+
+/*
+	By Osvaldas Valutis, www.osvaldas.info
+	Available for use under the MIT License
+*/
+
+;
+(function($, window, document, undefined) {
+    $.fn.doubleTapToGo = function(params) {
+        if (!('ontouchstart' in window) &&
+            !navigator.msMaxTouchPoints &&
+            !navigator.userAgent.toLowerCase().match(/windows phone os 7/i)) return false;
+
+        this.each(function() {
+            var curItem = false;
+
+            $(this).on('click', function(e) {
+                var item = $(this);
+                if (item[0] != curItem[0]) {
+                    e.preventDefault();
+                    curItem = item;
+                }
+            });
+
+            $(document).on('click touchstart MSPointerDown', function(e) {
+                var resetItem = true,
+                    parents = $(e.target).parents();
+
+                for (var i = 0; i < parents.length; i++)
+                    if (parents[i] == curItem[0])
+                        resetItem = false;
+
+                if (resetItem)
+                    curItem = false;
+            });
+        });
+        return this;
+    };
+})(jQuery, window, document);
+
+/**
+ * doubleTapToGoDecorator
+ * Adds the ability to remove the need for a second tap
+ * when in the mobile view
+ *
+ * @param {function} f - doubleTapToGo
+ */
+function doubleTapToGoDecorator(f) {
+    return function() {
+
+        this.each(function() {
+            $(this).on('click', function(e) {
+
+                // If mobile menu view
+                if ($('#menu-button').css('display') == 'block') {
+
+                    // If this is not a submenu button
+                    if (!$(e.target).hasClass('submenu-button')) {
+
+                        // Remove the need for a second tap
+                        window.location.href = $(e.target).attr('href');
+                    }
+                }
+
+            });
+        });
+
+        return f.apply(this);
+    }
+}
+
+// Add decorator to the doubleTapToGo plugin
+jQuery.fn.doubleTapToGo = doubleTapToGoDecorator(jQuery.fn.doubleTapToGo);
+
+/**
+ * jQuery
+ */
+(function($) {
+    $(document).ready(function() {
+
+        $("#cssmenu").menumaker({
+            title: "Menu",
+            format: "multitoggle"
+        });
+
+        $('#cssmenu li:has(ul)').doubleTapToGo();
+
+    });
+})(jQuery);
--- formacao/admin.php
+++ formacao/admin.php
@@ -20,56 +20,66 @@ $user = $_SESSION['user'];
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <!--
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    -->
 
-    <link rel="stylesheet" type="text/css" href="/Layout/css/admin.css";
-    <link href='https://fonts.googleapis.com/css?family=Sintony:400,700&subset=latin-ext' rel='stylesheet' type='text/css'>
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/Layout/css/menu.css";
 </head>
 
 <body>
-<!--
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
--->
-    <div class="container">
-        <header>
-            <h2>Formação PHP 5</h2>
-        </header>
-
-        <nav class="menu">
-            <ul>
-                <li class="active">
-                    <a href="#">Home</a>
-                </li>
-                <li class="drop-down">
-                    <a href="#">Products</a>
-
-                    <div class="mega-menu fadeIn animated">
-                        <div class="mm-6column">
-                            <span class="left-images">
-                                <img  src="https://api.codeexpertslearning.com.br/assets/img/courses/formacao-php-basico.jpg">
-                                <p>Most Popular</p>
-                            </span>
-                        </div>
-                    </div>
-                </li>
-
-                <li style="float: right;">
-                    <a href="/formacao/login/logout.php">Logout</a>
-                </li>
-                <li style="float: right;">
-                    Bem vindo, <?=ucwords(($user['name'] != null) ? $user['name'] : $user['NAME'])?>&nbsp
-                </li>
-        </nav>
+    <div id='cssmenu'>
+        <ul>
+            <li><a href='#'>Home</a></li>
+            <li class='active'><a href='https://s.codepen.io/dmitrykiselyov/debug/XJwqZM?SecondTap'>Products</a>
+                <ul>
+                    <li><a href='#Product 1'>Product 1</a>
+                        <ul>
+                            <li><a href='#SubProduct1'>Sub Product</a></li>
+                            <li><a href='#SubProduct2'>Sub Product</a></li>
+                        </ul>
+                    </li>
+                    <li><a href='#Product 2'>Product 2</a>
+                        <ul>
+                            <li><a href='#'>Sub Product</a></li>
+                            <li><a href='#'>Sub Product</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>Contact</a></li>
+            <li class='active' style="float: right; padding-right: 4.7em;">
+                <a href='https://s.codepen.io/dmitrykiselyov/debug/XJwqZM?SecondTap'>
+                    <?=ucwords(($user['name'] != null) ? $user['name'] : $user['NAME'])?>
+                </a>
+                <ul>
+                    <li><a href='#Profile'>Profile
+                            <svg class="icon">
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#users"></use>
+                            </svg>
+                        </a></li>
+                    <li><a href="/formacao/login/logout.php">Logout
+                            <svg class="icon">
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#exit"></use>
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
     </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="/Layout/javascript/teste.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="/Layout/javascript/menu.js"></script>
+
+    <svg xmlns="http://www.w3.org/2000/svg" class="icons">
+        <symbol id="exit" viewBox="0 0 32 32">
+            <title>exit</title>
+            <path d="M24 20v-4h-10v-4h10v-4l6 6zM22 18v8h-10v6l-12-6v-26h22v10h-2v-8h-16l8 4v18h8v-6z"></path>
+        </symbol>
+        <symbol id="users" viewBox="0 0 36 32">
+            <title>users</title>
+            <path d="M24 24.082v-1.649c2.203-1.241 4-4.337 4-7.432 0-4.971 0-9-6-9s-6 4.029-6 9c0 3.096 1.797 6.191 4 7.432v1.649c-6.784 0.555-12 3.888-12 7.918h28c0-4.030-5.216-7.364-12-7.918z"></path>
+            <path d="M10.225 24.854c1.728-1.13 3.877-1.989 6.243-2.513-0.47-0.556-0.897-1.176-1.265-1.844-0.95-1.726-1.453-3.627-1.453-5.497 0-2.689 0-5.228 0.956-7.305 0.928-2.016 2.598-3.265 4.976-3.734-0.529-2.39-1.936-3.961-5.682-3.961-6 0-6 4.029-6 9 0 3.096 1.797 6.191 4 7.432v1.649c-6.784 0.555-12 3.888-12 7.918h8.719c0.454-0.403 0.956-0.787 1.506-1.146z"></path>
+        </symbol>
+    </svg>
 </body>
 </html>
